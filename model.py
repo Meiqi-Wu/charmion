07df5210607aeeba91c8986c7b0ddab1b23eb55f
Meiqi-Wu <meiqiwu9878@gmail.com>
2020-10-04 22:09:33 -0400

    Early stopping and GPU

--- model.py
+++ model.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from callbacks import EarlyStopping
+from utils import make_dataloader
+from sklearn.model_selection import train_test_split
 
 SEED = 123
 np.random.seed(SEED)
@@ -24,7 +27,10 @@ class DenseBlock(nn.Module):
         x = self.batchnorm(x)
         x = self.linear(x)
         x = self.dropout(x)
-        return self.activate(x)
+        if self.activate:
+            return self.activate(x)
+        else:
+            return x
     
 class DenseNet(nn.Module):
     '''
@@ -41,7 +47,7 @@ class DenseNet(nn.Module):
         layer_size = [input_size] + hidden_size + [output_size]
         for i in range(len(layer_size)-2):
             self.layers.append(DenseBlock(layer_size[i], layer_size[i+1], self.dropout[i], F.relu))
-        self.layers.append(DenseBlock(layer_size[-2], layer_size[-1], 0, F.sigmoid))
+        self.layers.append(DenseBlock(layer_size[-2], layer_size[-1], 0, None))
 
     
     def forward(self, x):
@@ -57,13 +63,6 @@ class DenseNet(nn.Module):
 
         return L1*torch.norm(all_params, 1) + L2*torch.norm(all_params, 2)
     
-#         for layer in self.layers[:-2]:
-#             w = layer.linear.weight.data
-#             l1 += torch.sum(w.abs())
-#             l2 += torch.sum(torch.mul(w,w))
-#         w = self.layers[-1].weight.data
-#         l1 += torch.sum(w.abs())
-#         l2 += torch.sum(torch.mul(w,w))
         
         
 class Model(object):
@@ -73,47 +72,55 @@ class Model(object):
     def __init__(self, net):
         super(Model, self).__init__()
         self.net = net
-        
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda:0')
+        else:
+            self.device = torch.device('cpu')
+        self.net.to(self.device)
+       
 
-    def fit(self, X, y, epoch, lr, batch_size, L1, L2, pos_weight=1, verbose=True):
-        
-        n_samples = len(X)
+    def fit(self, X, y, epoch, lr, batch_size, L1, L2, patience=5, pos_weight=1, verbose=True):
         if type(X)!=np.ndarray:
             X = X.values
             y = y.values
-            
+        # split train and validation set
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+        X_val = torch.from_numpy(X_val).float().to(self.device)
+        y_val = torch.from_numpy(y_val).float().to(self.device)
+        train_loader = make_dataloader(X_train, y_train, batch_size)
+        
         optimizer = optim.Adam(self.net.parameters(), lr = lr)    
         criterian = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(pos_weight))
+        early_stopping = EarlyStopping(patience=patience, verbose=True)
         
+        n_features = X_train.shape[1]
         for e in range(epoch):
-            for i in range(0, n_samples, batch_size):
-                X_batch = X[i:min(i+batch_size, n_samples)]
-                y_target = y[i:min(i+batch_size, n_samples)]
-                X_batch = torch.from_numpy(X_batch).float()
-                y_target = torch.from_numpy(y_target).float()
-                
+            counter = 0
+            for data in train_loader:
+                counter += 1
+                X_batch = data[:,:n_features].float().to(self.device)
+                y_target = data[:,n_features:].float().to(self.device)
+
                 optimizer.zero_grad()
+                
                 y_prob = self.net(X_batch)
-                
-                y_prob = y_prob.view(-1,1)
-                y_target = y_target.view(-1,1)
-            
-                loss = criterian(y_prob, y_target) + self.net.regular_loss(L1, L2)
-                
+                loss = criterian(y_prob.view(-1,1), y_target.view(-1,1)) + self.net.regular_loss(L1, L2)
                 loss.backward()
-                
-#                 gradient = self.net.layers[0].linear.weight.grad
-#                 print(torch.min(gradient), torch.max(gradient))
-                
-#                 if (gradient != gradient).any():
-#                     print('gradient')
-#                     print(gradient)
-                        
                 optimizer.step()
-                
+    
                 if verbose:
-                    if i%(30*batch_size)==0:
-                        print(f"Epoch [{e+1}, {i}] : loss {loss}")       
+                    if counter % 40 ==0:
+                        print(f"Epoch [{e+1}, {counter}] : train loss {loss}") 
+                        
+            # validation loss
+            y_prob = self.net(X_val)
+            valid_loss = criterian(y_prob.view(-1,1), y_val.view(-1,1)) + self.net.regular_loss(L1, L2)
+            
+            early_stopping(valid_loss, self.net)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+        self.net.load_state_dict(torch.load('checkpoint.pt'))
     
     def predict(self, X, thresh = 0.5):
         prob = self.predict_prob(X)
@@ -126,6 +133,6 @@ class Model(object):
         elif type(X)== np.ndarray:
             X = torch.from_numpy(X).float()
         
-        output = self.net(X)
-        return output.detach().numpy()
+        output = F.sigmoid(self.net(X))
+        return output.detach().numpy().astype('float64')
 
